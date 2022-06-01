0015a159da49c36b9aae3948b176023a1b06fd36
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-02 00:07:38 +0100

    quiz interface

---
+++ app.js
@@ -0,0 +1,26 @@
+const quizData = [
+  {
+    question: "how old is Mr Dominic",
+    a: "12",
+    a: "27",
+    c: "45",
+    d: "105",
+    correct: "c",
+  },
+  {
+    question: "what is the most popular programming language",
+    a: "javascript",
+    b: "c++",
+    c: "python",
+    d: "java",
+    correct: "a",
+  },
+  {
+    question: "who is the president of Nigeria ",
+    a: "Goodluck Jonathan",
+    b: "Muhammedu Buhari",
+    c: "ochu dominic",
+    d: "peter Obi",
+    correct: "b",
+  },
+];
